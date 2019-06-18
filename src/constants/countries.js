04a26523c7205e7527b92bae2b11db522937e4cb
Veronica Mihai <mihaiveronica@ymail.com>
2019-06-18 19:33:14 +0100

    Constants --> countries list

---
+++ src/constants/countries.js
@@ -0,0 +1,22 @@
+// variables 
+export const countries = [{
+        code: 'gb',
+        name: 'Great Britain'
+    },
+    {
+        code: 'us',
+        name: 'United States'
+    },
+    {
+        code: 'fr',
+        name: 'France'
+    },
+    {
+        code: 'au',
+        name: 'Australia'
+    },
+    {
+        code: 'id',
+        name: 'India'
+    }
+];

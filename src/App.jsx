cf820305020ea6190add0c2f290dcb9dd8418f5d
Veronica Mihai <mihaiveronica@ymail.com>
2019-06-18 18:47:21 +0100

    News Dashboard --> render news dashboard

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Components
+import NewsDashboard from './features/news-dashboard/NewsDashboard.jsx';
+
+function App() {
+  return <NewsDashboard />;
+}
+
+export default App;

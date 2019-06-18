73bda00e41c75fdf8bb3577cb34a92d48832dcc9
Veronica Mihai <mihaiveronica@ymail.com>
2019-06-18 19:39:11 +0100

    Cleanup ---> App.css, add container component wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 // Components
 import NewsDashboard from './features/news-dashboard/NewsDashboard.jsx';
+import Container from '@material-ui/core/Container';
 
 function App() {
-  return <NewsDashboard />;
+  return (<Container> 
+    <NewsDashboard /> 
+  </Container>);
 }
 
 export default App;
